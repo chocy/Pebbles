dcb249616f1ba34be19c8bd726bf97329e160722
chocy <harithasri.j@gmail.com>
2018-07-01 19:32:46 -0600

    initial commit adding all files to the dev branch

---
+++ src/main/java/com/git/Pebbles/PebblesApplication.java
@@ -0,0 +1,12 @@
+package com.git.Pebbles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PebblesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PebblesApplication.class, args);
+	}
+}
